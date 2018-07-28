3e8af4576137d1477acc489e81279c7e9ba03a60
Thales Castro <28463839+cineasthales@users.noreply.github.com>
2018-07-28 20:28:03 -0300

    Tanques isa e thales

---
+++ eurobocode/Hurricane.java
@@ -0,0 +1,56 @@
+package eurobocode;
+import robocode.*;
+//import java.awt.Color;
+
+// API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
+
+/**
+ * Hurricane - a robot by (your name here)
+ */
+public class Hurricane extends Robot
+{
+	/**
+	 * run: Hurricane's default behavior
+	 */
+	public void run() {
+		// Initialization of the robot should be put here
+
+		// After trying out your robot, try uncommenting the import at the top,
+		// and the next line:
+
+		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+
+		// Robot main loop
+		while(true) {
+			// Replace the next 4 lines with any behavior you would like
+			turnGunRight(180);
+			ahead(50);
+			turnGunLeft(360);				
+			back(100);
+		}
+	}
+
+	/**
+	 * onScannedRobot: What to do when you see another robot
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// Replace the next line with any behavior you would like
+		fire(3);
+	}
+
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+		// Replace the next line with any behavior you would like
+		ahead(50);
+	}
+	
+	/**
+	 * onHitWall: What to do when you hit a wall
+	 */
+	public void onHitWall(HitWallEvent e) {
+		// Replace the next line with any behavior you would like		
+		turnGunRight(180);
+	}	
+}
